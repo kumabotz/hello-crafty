0cbc444acff66018b7af4b6e822caf2eada547e5
Tery Lim <kumabotz@gmail.com>
2013-10-27 20:32:45 +0800

    Rearrange for readability

--- src/game.js
+++ src/game.js
@@ -32,5 +32,9 @@ Game = {
   }
 };
 
-$text_css = { 'font-size': '24px', 'font-family': 'Arial', 'color': 'white',
-    'text-align': 'center' }
+$text_css = {
+  'font-size': '24px',
+  'font-family': 'Arial',
+  'color': 'white',
+  'text-align': 'center'
+}
